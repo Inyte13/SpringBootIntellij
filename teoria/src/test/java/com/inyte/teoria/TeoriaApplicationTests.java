97b86a917387bcef912f8a4e50e75004d07eb293
Inyte13 <luisgardaz13@gmail.com>
2025-11-29 21:01:51 -0500

    springboot

---
+++ teoria/src/test/java/com/inyte/teoria/TeoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inyte.teoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
